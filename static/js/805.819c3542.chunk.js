456d4544ab73d6217cc566a8d0356eb4b46bb9ca
Oleksandr-Karpiuk <113094326+Oleksandr-Karpiuk@users.noreply.github.com>
2023-05-19 08:58:30 +0000

    Deploying to gh-pages from @ Oleksandr-Karpiuk/goit-react-hw-05-movies@fc17d36b53f3e8b992fc3a3dda02547f9960a2fd 🚀

---
+++ static/js/805.819c3542.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("h2",{children:"Sorry, we couldn't find that page"})}}}]);
+//# sourceMappingURL=805.819c3542.chunk.js.map
